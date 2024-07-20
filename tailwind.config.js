a6d1f9af2bdef76c2b18a87c5f1b926a597cfed7
Aaryan Narayan <aaryan.narayan@outlook.com>
2024-07-20 16:47:06 +1000

    Working on the navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./src/**/*.{html,js,svelte,ts}'],
-    theme: {
-        extend: {}
-    },
-    plugins: []
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		extend: {
+			fontFamily: {
+				opensans: ["open-sans"]
+			}
+		}
+	},
+
+	plugins: [require('@tailwindcss/typography')]
 };
