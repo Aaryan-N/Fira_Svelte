defed8e6d9188c90f22f54c62c846158e01c8c94
Aaryan Narayan <aaryan.narayan@outlook.com>
2024-07-20 13:17:41 +1000

    Working on the navbar

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,12 @@
+<nav class="bg-[#FFE7BC] mb-2">
+    <div class="justify-start flex flex-row p-2 m-2">
+        <p class="p-2">rah</p>
+    </div>
+    <div class="justify-end flex flex-row p-2 m-2">
+            <a href="/">Home</a>
+            <a href="/features">Features</a>
+            <a href="/docs">Documentation</a>
+            <a href="/about">About Us</a>
+            <a href="/contactus">Contact</a>
+    </div>
+</nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,2 @@
-<h1 class="text-3xl font-bold underline">
-    Hello world!
-</h1>
-
-<script>
-    import Layout from './layout/+layout.svelte'
-    let name = 'svelte';
-</script>
-
-<h1>Your name is {name.toUpperCase()}</h1>
-
-<Layout />
-
-<style>
-    p{
-        font-family: sans-serif;
-        color: goldenrod;
-        font-size: 2em;
-    }
-</style>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
