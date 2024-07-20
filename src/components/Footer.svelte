869d9fc5a91bb517963ddbdd7b67d7c96863667d
Aaryan Narayan <aaryan.narayan@outlook.com>
2024-07-20 22:10:51 +1000

    Finished up the footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -2,9 +2,11 @@
     const copyrightYear = new Date;
 </script>
 <footer class="w-full h-auto bg-gradient-to-t from-[#302134] to-[#412534] flex-1">
-    <div class="pt-2 pl-2 flex items-center w-full">
+    <div class="pt-2 pl-2  w-full">
+        <a href="/" class="flex items-center">
         <img class="w-[75px] h-[75px] rounded-full float-left" src="/FiraLogo.jpeg" alt="Fira Logo">
         <p class="pl-2 text-xl text-[#FFE7BC]">Fira</p>
+        </a>
     </div>
 
     <hr class="my-6 border-[#FFE7BC] ml-1.5 mr-1.5" />
@@ -14,16 +16,19 @@
             <p class="mb-4 text-[#FFE7BC] text-lg">Fira</p>
             <ul class="font-medium">
             <li class="mb-4 text-[#ccb997] text-sm">
-                <a href="/">Features</a>
+                    <a href="/features">Home</a>
             </li>
             <li class="mb-4 text-[#ccb997] text-sm">
-                <a href="/">Documentation</a>
+                <a href="/features">Features</a>
             </li>
             <li class="mb-4 text-[#ccb997] text-sm">
-                    <a href="/">About Us</a>
+                <a href="/docs">Documentation</a>
             </li>
             <li class="mb-4 text-[#ccb997] text-sm">
-                    <a href="/">Contact Us</a>
+                    <a href="/about">About Us</a>
+            </li>
+            <li class="mb-4 text-[#ccb997] text-sm">
+                    <a href="/contactus">Contact Us</a>
             </li>
             </ul>
         </div>
@@ -31,7 +36,7 @@
             <p class="mb-4 text-[#FFE7BC] text-lg">Socials</p>
             <ul class="font-medium">
                 <li class="mb-4 text-[#ccb997] text-sm">
-                    <a href="/">
+                    <a class="flex items-center" href="/">
                     <svg class="w-4 h-4 float-left" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 8 19">
                         <path fill-rule="evenodd" d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" clip-rule="evenodd"/>
                     </svg>
@@ -39,21 +44,23 @@
                     </a>
                 </li>
                 <li class="mb-4 text-[#ccb997] text-sm">
-                    <a href="/">
-                        <svg class="w-4 h-4 float-left mr-1" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 1668.56 1221.19" viewBox="0 0 1668.56 1221.19" id="twitter-x"><path d="M283.94,167.31l386.39,516.64L281.5,1104h87.51l340.42-367.76L984.48,1104h297.8L874.15,558.3l361.92-390.99
-		h-87.51l-313.51,338.7l-253.31-338.7H283.94z M412.63,231.77h136.81l604.13,807.76h-136.81L412.63,231.77z" transform="translate(52.39 -25.059)"></path></svg>
+                    <a class="flex items-center" href="/">
+                        <svg class="w-4 h-4 mr-1" xmlns="http://www.w3.org/2000/svg" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd" viewBox="0 0 512 462.799"><path fill="#fff" fill-rule="nonzero" d="M403.229 0h78.506L310.219 196.04 512 462.799H354.002L230.261 301.007 88.669 462.799h-78.56l183.455-209.683L0 0h161.999l111.856 147.88L403.229 0zm-27.556 415.805h43.505L138.363 44.527h-46.68l283.99 371.278z"/></svg>
                         <p>Twitter</p>
                     </a>
                 </li>
                 <li class="mb-4 text-[#ccb997] text-sm flex">
-                    <a href="/">Youtube</a>
+                    <a href="/" class="flex items-center">
+                        <svg class="w-4 h-4 mr-1" xmlns="http://www.w3.org/2000/svg" height="800" width="1200" viewBox="-35.20005 -41.33325 305.0671 247.9995"><path d="M93.333 117.559V47.775l61.334 34.893zm136.43-91.742c-2.699-10.162-10.651-18.165-20.747-20.881C190.716 0 117.333 0 117.333 0S43.951 0 25.651 4.936C15.555 7.652 7.603 15.655 4.904 25.817 0 44.236 0 82.667 0 82.667s0 38.429 4.904 56.849c2.699 10.163 10.65 18.165 20.747 20.883 18.3 4.934 91.682 4.934 91.682 4.934s73.383 0 91.683-4.934c10.096-2.718 18.048-10.72 20.747-20.883 4.904-18.42 4.904-56.85 4.904-56.85s0-38.43-4.904-56.849" fill="#fff"/></svg>
+                        <p>Youtube</p>
+                    </a>
                 </li>
                 <li class="mb-4 text-[#ccb997] text-sm">
-                    <a href="/">
+                    <a class="flex items-center" href="/">
                     <svg class="w-4 h-4 float-left mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 21 16">
                         <path d="M16.942 1.556a16.3 16.3 0 0 0-4.126-1.3 12.04 12.04 0 0 0-.529 1.1 15.175 15.175 0 0 0-4.573 0 11.585 11.585 0 0 0-.535-1.1 16.274 16.274 0 0 0-4.129 1.3A17.392 17.392 0 0 0 .182 13.218a15.785 15.785 0 0 0 4.963 2.521c.41-.564.773-1.16 1.084-1.785a10.63 10.63 0 0 1-1.706-.83c.143-.106.283-.217.418-.33a11.664 11.664 0 0 0 10.118 0c.137.113.277.224.418.33-.544.328-1.116.606-1.71.832a12.52 12.52 0 0 0 1.084 1.785 16.46 16.46 0 0 0 5.064-2.595 17.286 17.286 0 0 0-2.973-11.59ZM6.678 10.813a1.941 1.941 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.919 1.919 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Zm6.644 0a1.94 1.94 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.918 1.918 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Z"/>
                     </svg>
-                    <p class="ml-1">Discord</p>
+                    <p>Discord</p>
                     </a>
                 </li>
             </ul>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,6 @@
     <p class="text-[#FFE7BC] mt-2 flex items-center justify-center text-3xl">The next generation of Discord Bot. Never invite another bot again!</p>
     <div class="mt-4 justify-center flex">
         <a href="/features"><button class="h-[45px] bg-[#FFE7BC] text-black px-5 py-2 rounded-full hover:bg-[#ffffff] mr-2.5">Features</button></a>
-        <a href="/docs"><button class="h-[45px] bg-[#FFE7BC] text-black px-5 py-2 rounded-full hover:bg-[#ffffff] mr-2.5">Documentation</button></a>
+        <a href="/docs"><button class="h-[45px] bg-[#FFE7BC] text-black px-5 py-2 rounded-full hover:bg-[#ffffff] mr-2.5">Invite to your server!</button></a>
     </div>
 </div>
