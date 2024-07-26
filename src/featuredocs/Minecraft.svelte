bf17b38fbfd24db6711ca9d83c61fa4479e45516
Aaryan Narayan <aaryan.narayan@outlook.com>
2024-07-27 00:26:51 +1000

    Well im trying to get this accordion to work but its playing me instead

--- src/featuredocs/Minecraft.svelte
+++ src/featuredocs/Minecraft.svelte
@@ -1,8 +1,6 @@
 <script>
-    let motdParameterDropdown = false;
-    function handleClick(event){
-        return motdParameterDropdown ? true : false;
-    }
+    let motdParameterDropdown = true;
+    let motdParameterDropdownHidden = true;
 </script>
 <div>
     <h1 class="text-xl text-[#FFE7BC] ml-1 mb-1">Minecraft Commands</h1>
@@ -18,11 +16,16 @@
     </div>
 
     <div class="ml-1 mt-1 bg-[#302134] w-full h-auto flex px-2 rounded-xl container">
-        <span class="inline-flex items-center justify-between w-full rounded py-2 px-2">
-            <span class="text-[#FFE7BC]">Parameters</span>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="white" class="w-4 h-4">
-            <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14Zm.75-10.25a.75.75 0 0 0-1.5 0v4.69L6.03 8.22a.75.75 0 0 0-1.06 1.06l2.5 2.5a.75.75 0 0 0 1.06 0l2.5-2.5a.75.75 0 1 0-1.06-1.06L8.75 9.44V4.75Z" clip-rule="evenodd" />
-            </svg>
-        </span>
-    </div>
+            <div class="inline-flex items-center justify-between w-full rounded py-2 px-2" aria-hidden="true" on:keydown={() => motdParameterDropdown = !motdParameterDropdown} on:click={() => motdParameterDropdown = !motdParameterDropdown}>
+                <button class="text-[#FFE7BC] cursor-pointer p-[2px] w-[100%] text-left border-none duration-300">Section 1</button>
+                {#if motdParameterDropdown}
+                    {motdParameterDropdownHidden = "hidden"}
+                {:else}
+                    {motdParameterDropdownHidden = ""}
+                {/if}
+                <div class="{motdParameterDropdownHidden}">
+                    <p>Lorem ipsum...</p>
+                </div>
+        </div>
+</div>
 </div>
