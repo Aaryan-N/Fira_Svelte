2b068ae1a3a231238cfc81290a0b94ef2646aa8c
Aaryan Narayan <aaryan.narayan@outlook.com>
2024-07-27 08:56:55 +1000

    ALRIGHT THE ACCORDIAN WORKS!

--- src/featuredocs/Minecraft.svelte
+++ src/featuredocs/Minecraft.svelte
@@ -1,10 +1,21 @@
 <script>
     let motdParameterDropdown = true;
-    let motdParameterDropdownHidden = "";
-    import { fade } from 'svelte/transition';
+    import { slide, spin } from 'svelte/transition'
+
+    function spin(node, options) {
+        const {times = 0.5} = options;
+        return {
+            ...options,
+            css(t) {
+                const degrees = 360 * times;
+                return `transform: scale(${t}) rotate(${t * degrees}deg);`;
+            }
+        };
+    }
+
+
 </script>
 <div>
-    {fade}
     <h1 class="text-xl text-[#FFE7BC] ml-1 mb-1">Minecraft Commands</h1>
     <hr class="border-[#FFE7BC] ml-1 mr-1.5 mb-1" />
     <div class="flex justify-start border-[#FFE7BC] w-auto h-auto ml-1">
@@ -25,14 +36,16 @@
                     <path stroke-linecap="round" stroke-linejoin="round" d="m9 12.75 3 3m0 0 3-3m-3 3v-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"></path>
                 </svg>
             </div>
-            <div class="{ motdParameterDropdown === true ? 'hidden' : '' }">
+            {#if motdParameterDropdown}
+            <div>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-5 h-5">
                     <path stroke-linecap="round" stroke-linejoin="round" d="m15 11.25-3-3m0 0-3 3m3-3v7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"></path>
                 </svg>
             </div>
-
+            {/if}
         </div>
-        <div class="{ motdParameterDropdown === true ? 'hidden transition ease-in-out delay-150' : 'transition ease-in-out delay-150' }">
+        {#if motdParameterDropdown}
+            <div transition:slide>
             <hr class="border-[#FFE7BC] pl-0.5 ml-2 mr-1.5 mb-1" />
             <div class="text-sm pt-1 ml-2 mb-2 flex text-[#FFE7BC]">
                 <ul>
@@ -41,6 +54,7 @@
                 </li>
                 </ul>
             </div>
-        </div>
+            </div>
+        {/if}
 </div>
 </div>
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,3 @@
-import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,10 +7,7 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
-	},
-
-	preprocess: [mdsvex()],
-	extensions: ['.svelte', '.svx']
+	}
 };
 
 export default config;
