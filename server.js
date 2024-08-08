be1838b74d38d1cbf20b79919ed6bab1d1879f59
Aaryan Narayan <aaryan.narayan@outlook.com>
2024-08-09 00:24:59 +1000

    ALRIGHTTTT ROUTING WORKSSSS

--- server.js
+++ server.js
@@ -1,39 +1,49 @@
-import 'dotenv/config';
-import Koa from 'koa';
-import Router from '@koa/router';
-import cors from '@koa/cors';
-import bodyParser from 'koa-bodyparser';
-import axios from 'axios';
-import { guildSchemaExport } from './src/models/guildModel.js';
+import express from 'express';
 import { handler } from './build/handler.js'
+import 'dotenv/config'
+import axios from 'axios';
 
-const app = new Koa();
-const router = new Router();
+const app = express();
+const port = 3000;
 
-app.use(cors());
-app.use(bodyParser());
-app.use(router.routes());
-app.use(router.allowedMethods());
-app.use(handler)
-
-// the relevant query: {"guildId": {"$in": ["848070581731328010", "435465777754323"]}}
-
-console.log("Serving Logins!")
-
-router.get('/auth/discord/login', async ctx => {
+app.get("/auth/discord/login", (req, res) => {
 	console.log("Triggered!")
-	const url =
-		'https://discord.com/oauth2/authorize?client_id=782048237170589696&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A4000%2Fauth%2Fdiscord%2Fcallback&scope=guilds';
-	ctx.redirect(url);
-});
-
-router.get('/auth/discord/callback', async ctx => {
-	if (!ctx.query.code) throw new Error("what the sigma");
-	const { code } = ctx.query;
-	const params = new URLSearchParams({
-		client_id: guildSchemaExport.clientId,
-	})
-	const response = await axios({})
+	const url = process.env.DISCORD_URL
+	res.redirect(url);
 })
 
-app.listen(5173);
+app.get('/auth/discord/callback', async (req, res) => {
+	if (!req.query.code) throw new Error("what the sigma");
+	const { code } = req.query;
+	res.send(code);
+
+	const params = new URLSearchParams({
+		client_id: process.env.DISCORD_CLIENT_ID,
+		client_secret: process.env.DISCORD_CLIENT_SECRET,
+		grant_type: 'authorization_code',
+		code,
+		redirect_uri: process.env.DISCORD_REDIRECT_URI
+	});
+
+	const headers = {
+		'Content-Type': 'application/x-www-form-urlencoded',
+		'Accept-Encoding': 'application/x-www-form-urlencoded'
+	};
+
+	const response = await axios.post(
+		'https://discord.com/api/oauth2/token',
+		params,
+		{
+			headers
+		}
+	);
+
+	console.log(response.data)
+	
+})
+
+app.use(handler)
+
+app.listen(port, () => {
+	console.log("Server running and has started on port: " + port);
+})
