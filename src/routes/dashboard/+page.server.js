30353f9698526fd9d3a4905124d1d63cda1111c2
Aaryan Narayan <aaryan.narayan@outlook.com>
2024-08-09 17:04:09 +1000

    Alright the auth works for now, now have to implement encryption

--- src/routes/dashboard/+page.server.js
+++ src/routes/dashboard/+page.server.js
@@ -1,3 +1,10 @@
+import { redirect } from '@sveltejs/kit'
+
+
 export const load = async ({ locals }) => {
-	console.log(locals);
+	console.log(locals.user)
+	if (!locals.user) {
+		console.log("rip call mr beast")
+		throw redirect(303, "/")
+	}
 }
